845fd7214d41e6201ca3e9d477ff8ce0c780c8e1
Blasb9737 <blasb123real@gmail.com>
2026-05-16 14:53:12 +1200

    created features folder in app directory
    created features folder in app directory

---
+++ app/features/register/page.tsx
@@ -0,0 +1,15 @@
+import { RegistrationForm } from "@/components/registration/registration-form";
+
+export default function Page() {
+    return (
+        <main>
+            <h1>hellow</h1>
+             {/* square registry and everythings on */} 
+            <div id = "box" >
+
+            </div>
+
+            {/* <RegistrationForm /> */}
+        </main>
+    )
+}
